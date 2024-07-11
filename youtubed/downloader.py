281822c6d0851b0c4eda930a741f8c241f4d2fc6
Mauricio Bellon <mauriciobellon@gmail.com>
2024-07-11 16:52:43 -0300

    feat: Update youtubed CLI command in pyproject.toml and fix audio extraction bug
    - Update the youtubed CLI command in pyproject.toml from "youtubed.cli:youtubed" to "youtubed.cli:cli" to match the correct entry point.
    - Fix a bug in the AudioExtractor class where the extracted audio file was not being saved with the correct file extension.

---
+++ youtubed/downloader.py
@@ -0,0 +1,34 @@
+import os
+from yt_dlp import YoutubeDL
+
+class Downloader:
+    def __init__(self, output_path, cookie_file='cookies.txt'):
+        self.output_path = output_path
+        self.ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',
+            'merge_output_format': 'mp4',
+            'cookiefile': cookie_file,
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+            }
+        }
+
+    def download_video(self, url, playlist_title=None):
+        self.ydl_opts['outtmpl'] = os.path.join(self.output_path, '%(uploader)s', '%(playlist_title)s', '%(title)s', '%(title)s.%(ext)s') if playlist_title else os.path.join(self.output_path, '%(uploader)s', '%(title)s', '%(title)s.%(ext)s')
+        with YoutubeDL(self.ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            title = info_dict.get('title', None)
+            uploader = info_dict.get('uploader', 'UnknownUploader')
+            playlist_title = info_dict.get('playlist_title', playlist_title)
+            filename = ydl.prepare_filename(info_dict).replace('.webm', '.mp4').replace('.mkv', '.mp4')
+        return filename, title, uploader, playlist_title
+
+    def download_playlist(self, url):
+        self.ydl_opts['extract_flat'] = True
+        with YoutubeDL(self.ydl_opts) as ydl:
+            playlist_dict = ydl.extract_info(url, download=False)
+            playlist_title = playlist_dict.get('title', 'UnknownPlaylist')
+            video_urls = [f"https://www.youtube.com/watch?v={entry['id']}" for entry in playlist_dict['entries']]
+        return video_urls, playlist_title
